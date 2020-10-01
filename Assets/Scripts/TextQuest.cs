eb26b21226ec9660c2ed66fec935f4c729daaa88
Kemal <45937663+BesBlet@users.noreply.github.com>
2020-10-01 20:57:10 +0300

    Initial commit

---
+++ Assets/Scripts/TextQuest.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextQuest : MonoBehaviour
+{
+    public Text contentText;
+    public Step startStep;
+    Step currentStep;
+    void Start()
+    {
+        currentStep = startStep;
+        contentText.text = startStep.content;
+    }
+
+ 
+    void Update()
+    {
+       if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            currentStep = currentStep.nextSteps[0];
+            contentText.text = currentStep.content;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            currentStep = currentStep.nextSteps[1];
+            contentText.text = currentStep.content;
+        }
+    }
+}
